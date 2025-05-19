93f8d22dd6d059561730c717cbf4603828ca1841
mohamed khaled <first99ali@gmail.com>
2025-05-19 15:44:54 +0300

    Project Setup

---
+++ BackEnd/DoctorAppointmentsSystem/Persistence/Data/Seeding/ReadMe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Data.Seeding
+{
+    internal class ReadMe
+    {
+
+        ////in seeding folder 
+        ///please add json files for seeding data   
+        ///only add json files !!!!!!!!!!!!!!!!!
+    }
+}
