78a08404975eab60dbec2784a5661ed1e9420118
mohamed khaled <first99ali@gmail.com>
2025-05-25 01:25:06 +0300

    Add ServiceManager and IServiceManager for service access
    Introduced `IServiceManager` interface in `Services.Abstraction` to
    define properties for accessing core services. Added `ServiceManager`
    class in `Services` namespace to implement the interface, using
    `Lazy<T>` for efficient service instantiation. Services include
    `IPatientService`, `IAuthenticationService`, and `IEmailService`.
    Dependencies like `IUnitOfWork`, `UserManager<AppUser>`, and
    `IConfiguration` are injected into `ServiceManager` to initialize
    services. Commented-out services indicate future plans or unused code.

---
+++ BackEnd/DoctorAppointmentsSystem/Services.Abstraction/IServiceManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Abstraction
+{
+    public interface IServiceManager
+    {
+        IPatientService PatientService { get; }
+        //IDoctorService DoctorService { get; }
+        IAuthenticationService  AuthenticationService { get; }
+        IEmailService EmailService { get; }
+        //IAppointmentService AppointmentService { get; }
+        //IFileService FileService { get; }
+        //INotificationService NotificationService { get; }
+
+    }
+}
---
+++ BackEnd/DoctorAppointmentsSystem/Services/ServiceManager.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Domain.Contracts;
+using Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Services.Abstraction;
+using Shared.Authentication;
+
+namespace Services
+{
+    public class ServiceManager : IServiceManager
+    {
+        
+        private readonly Lazy<IPatientService> _patientService;
+
+        private readonly Lazy<IAuthenticationService> _authenticationService;
+        private readonly Lazy<IEmailService>  _emailService;
+
+
+
+        public ServiceManager(IUnitOfWork unitOfWork, UserManager<AppUser> userManager, IMapper mapper,
+            IOptions<JWTOptions> options, IConfiguration configuration)
+        {
+            _patientService = new Lazy<IPatientService>(() => new PatientService(unitOfWork,mapper));
+            _emailService = new Lazy<IEmailService>(() => new EmailService(configuration));
+            _authenticationService = new Lazy<IAuthenticationService>(() => new AuthenticationService(userManager,unitOfWork, options,configuration ,_emailService.Value));
+
+        }
+        public IPatientService PatientService => _patientService.Value;
+
+        public IAuthenticationService AuthenticationService => _authenticationService.Value;
+
+        public IEmailService EmailService => _emailService.Value;
+    }
+}
