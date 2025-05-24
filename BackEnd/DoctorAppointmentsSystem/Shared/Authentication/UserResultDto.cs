8192ecb1d070752cab6753b324b509e296da5f1b
mohamed khaled <first99ali@gmail.com>
2025-05-25 01:37:12 +0300

    Add DTOs and error models for authentication and validation
    Added new DTOs in the `Shared.Authentication` namespace:
    - `DoctorRegisterDto`: Empty record.
    - `LoginDto`: Includes `Email` and `Password` properties.
    - `RegisterDto`: Includes properties with validation for user registration.
    - `UserResultDto`: Includes `Email` and `Token` properties.
    
    Added error models in the `Shared.ErrorModels` namespace:
    - `ErrorDetails`: Represents error details with JSON serialization.
    - `ValidationError`: Represents field-specific validation errors.
    - `ValidationErrorsResponse`: Aggregates validation errors with status code.

---
+++ BackEnd/DoctorAppointmentsSystem/Shared/Authentication/UserResultDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Authentication
+{
+    public record UserResultDto (string Email, 
+        //string DisplayName,
+        string Token);
+   
+}
