b438f387b2e62bc6ec5cab2a35155586dfe45fba
mohamed khaled <first99ali@gmail.com>
2025-05-25 01:54:01 +0300

    Add Swagger, Identity, and global error handling
    - Integrated Swagger for API documentation and UI generation.
    - Configured ASP.NET Core Identity with custom password policies.
    - Implemented JWT-based authentication with token validation.
    - Added custom validation error handling via ApiResponseFactory.
    - Created ExceptionHandlingMiddleware for global error handling.
    - Configured AppDbContext with SQL Server and DI for services.
    - Enhanced HTTP pipeline with middleware for security and errors.
    - Updated appsettings.json with connection strings and JWT options.
    - Improved error models for structured validation and exception responses.

---
+++ BackEnd/DoctorAppointmentsSystem/DoctorAppointmentsSystem.Web/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,83 @@
+﻿using Domain.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Shared.ErrorModels;
+using System;
+using System.Net;
+
+namespace DoctorAppointmentsSystem.Web.Middlewares
+{
+
+    internal class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next?.Invoke(httpContext);
+
+                if (httpContext.Response.StatusCode == (int)HttpStatusCode.NotFound)
+                    await HandleNotFoundEndPointException(httpContext);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                await HandleExceptionAsync(e, httpContext);
+            }
+        }
+
+        private async Task HandleExceptionAsync(Exception exception, HttpContext httpContext)
+        {
+
+            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;//500
+            httpContext.Response.ContentType = "application/json";
+
+            var response = new ErrorDetails
+            {
+                ErrorMessage = exception.Message,
+
+            };
+            httpContext.Response.StatusCode = exception switch
+            {
+                NotFoundException => (int)HttpStatusCode.NotFound,
+                UnAuthorizedException => (int)HttpStatusCode.Unauthorized,
+                ValidationException validationException => HandleValidationException(response, validationException),
+
+                _ => (int)HttpStatusCode.InternalServerError
+            };
+
+            response.StatusCode = httpContext.Response.StatusCode;
+            await httpContext.Response.WriteAsync(response.ToString());
+
+        }
+
+        private int HandleValidationException(ErrorDetails response, ValidationException validationException)
+        {
+            response.Errors = validationException.Errors;
+            return (int)HttpStatusCode.BadRequest;
+        }
+
+        private async Task HandleNotFoundEndPointException(HttpContext httpContext)
+        {
+
+            httpContext.Response.ContentType = "application/json";
+            var response = new ErrorDetails
+            {
+                StatusCode = httpContext.Response.StatusCode,
+                ErrorMessage = $"The EndPoint {httpContext.Request.Path} is not Found"
+
+            };
+
+            await httpContext.Response.WriteAsync(response.ToString());
+
+        }
+    }
+}
--- BackEnd/DoctorAppointmentsSystem/DoctorAppointmentsSystem.Web/Program.cs
+++ BackEnd/DoctorAppointmentsSystem/DoctorAppointmentsSystem.Web/Program.cs
@@ -1,6 +1,19 @@
 
+using DoctorAppointmentsSystem.Web.Factories;
+using DoctorAppointmentsSystem.Web.Middlewares;
 using Domain.Contracts;
+using Domain.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Persistence;
 using Persistence.Repositories;
+using Services;
+using Services.Abstraction;
+using Shared.Authentication;
+using System.Text;
 
 namespace DoctorAppointmentsSystem.Web
 {
@@ -15,11 +28,82 @@ namespace DoctorAppointmentsSystem.Web
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+            builder.Services.AddSwaggerGen();
+
+
+            //For Validation
+            builder.Services.Configure<ApiBehaviorOptions>(op =>
+            {
+                op.InvalidModelStateResponseFactory = ApiResponseFactory.CustomValidationErrors;
+            });
+
+
+            #region Identity
+            builder.Services.AddIdentity<AppUser, IdentityRole<int>>(op =>
+            {
+                op.Password.RequireLowercase = false;
+                op.Password.RequireNonAlphanumeric = false;
+                op.Password.RequireLowercase = false;
+                op.Password.RequireDigit = false;
+                op.Password.RequiredLength = 8;
+            }).AddDefaultTokenProviders()
+            .AddEntityFrameworkStores<AppDbContext>();
+            #endregion
+
+
+            #region DbContext
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+            #endregion
 
             #region IUnitOfWork
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
             builder.Services.AddScoped(typeof(IGenericRepository<,>), typeof(GenericRepository<,>));
             #endregion
+
+            #region AutoMapper
+            builder.Services.AddAutoMapper(typeof(Services.AssemblyReference).Assembly);
+            #endregion
+
+            #region Persentation
+            builder.Services.AddControllers().AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly);
+            #endregion
+
+
+            #region Authentication
+            builder.Services.Configure<JWTOptions>(builder.Configuration.GetSection("JwtOptions"));
+
+
+            builder.Services.AddAuthentication(
+                op =>
+                {
+                    op.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    op.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                }
+                ).AddJwtBearer(op =>
+                {
+
+                    var jwtOptions = builder.Configuration.GetSection("JwtOptions").Get<JWTOptions>();
+                    op.TokenValidationParameters = new()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = jwtOptions.Issuer,
+                        ValidAudience = jwtOptions.Audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SecretKey)),
+
+                    };
+                });
+            #endregion
+
+
+            builder.Services.AddScoped<IServiceManager, ServiceManager>();
+
+
 
             var app = builder.Build();
 
@@ -27,7 +111,12 @@ namespace DoctorAppointmentsSystem.Web
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
+
+            app.UseMiddleware<ExceptionHandlingMiddleware>();
+
 
             app.UseHttpsRedirection();
 
