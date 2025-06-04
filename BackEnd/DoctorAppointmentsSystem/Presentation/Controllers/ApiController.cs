b070117db4575ae2e72d929d3b6beb427fe2d2f4
mohamed khaled <first99ali@gmail.com>
2025-06-04 22:16:32 +0300

    Add response type attributes to ApiController
    Updated `using` directives to include `Shared.ErrorModels` and `System.Net` for handling error models and HTTP status codes. Added `[ProducesResponseType]` attributes to specify possible API responses:
    - `404 Not Found` with `ErrorDetails`
    - `500 Internal Server Error` with `ErrorDetails`
    - `400 Bad Request` with `ErrorDetails`
    
    Left a commented-out `[ProducesResponseType]` for `ProductResultDTO` and `HttpStatusCode.OK` unchanged for potential future use.

--- BackEnd/DoctorAppointmentsSystem/Presentation/Controllers/ApiController.cs
+++ BackEnd/DoctorAppointmentsSystem/Presentation/Controllers/ApiController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shared.ErrorModels;
+using System.Net;
 
 namespace Presentation.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [ProducesResponseType(typeof(ErrorDetails), (int)HttpStatusCode.NotFound)]
+    [ProducesResponseType(typeof(ErrorDetails), (int)HttpStatusCode.InternalServerError)]
+    [ProducesResponseType(typeof(ErrorDetails), (int)HttpStatusCode.BadRequest)]
+    //[ProducesResponseType(typeof(ProductResultDTO), (int)HttpStatusCode.OK)] // Uncomment if you have a specific DTO to return
     public class ApiController : ControllerBase
     {
     }
