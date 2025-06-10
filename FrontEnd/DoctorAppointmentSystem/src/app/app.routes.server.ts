c324705fc10a1f08fa518d28bd3e685ac073cb62
mohamed khaled <first99ali@gmail.com>
2025-06-10 23:50:45 +0300

    feat(routes): add server route for doctor profile

--- FrontEnd/DoctorAppointmentSystem/src/app/app.routes.server.ts
+++ FrontEnd/DoctorAppointmentSystem/src/app/app.routes.server.ts
@@ -4,5 +4,12 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
-  }
+  },
+  
+  
+    {
+    path: 'profile/doctor/:id',
+    renderMode:RenderMode.Server
+  },
+  
 ];
