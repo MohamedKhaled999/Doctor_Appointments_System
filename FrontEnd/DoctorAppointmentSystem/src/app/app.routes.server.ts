a7a85e0abcd9dc45f2e47bb8e77c6d87c759281b
mohamed khaled <first99ali@gmail.com>
2025-06-11 04:14:21 +0300

    fix(routes): change render mode for catch-all route to Prerender and restore doctor profile route

--- FrontEnd/DoctorAppointmentSystem/src/app/app.routes.server.ts
+++ FrontEnd/DoctorAppointmentSystem/src/app/app.routes.server.ts
@@ -3,13 +3,13 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Prerender
   },
   
   
-  //   {
-  //   path: 'profile/doctor/:id',
-  //   renderMode:RenderMode.Server
-  // },
+    {
+    path: 'profile/doctor/:id',
+    renderMode:RenderMode.Server
+  },
   
 ];
