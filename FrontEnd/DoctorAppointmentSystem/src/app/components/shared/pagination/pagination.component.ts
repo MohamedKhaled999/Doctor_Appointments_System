709d93bbfde587b5068f41e5780b45a3ffc25f6e
AhmedYasserMohammed <ahmedyasser200220@gmail.com>
2025-06-12 17:54:32 +0300

    feat: implement responsive maxSize adjustment in pagination component for improved usability on different screen sizes

--- FrontEnd/DoctorAppointmentSystem/src/app/components/shared/pagination/pagination.component.ts
+++ FrontEnd/DoctorAppointmentSystem/src/app/components/shared/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges, HostListener } from '@angular/core';
 import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 
@@ -15,8 +15,26 @@ export class PaginationComponent implements OnChanges {
   @Output() pageIndexChange = new EventEmitter<number>();
 
   pageIndex: number = 1;
-
   pageSize: number = 6;
+  maxSize: number = 5;  // Default maxSize
+
+  constructor() {
+    this.updateMaxSize();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.updateMaxSize();
+  }
+
+  private updateMaxSize() {
+    if(window.innerWidth > 576)
+      this.maxSize = 5
+    else if(window.innerWidth < 576 && window.innerWidth > 370)
+      this.maxSize = 3  // Change to 3 on small screens
+    else if (window.innerWidth < 370)
+      this.maxSize = 2
+  }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['currentPage']) {
