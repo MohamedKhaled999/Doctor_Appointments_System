1ecd4780ae3c994dd5635c8ea7a149f94672293c
Galal Mohammed <galalmoh2001@gmail.com>
2025-07-08 13:27:40 +0300

    fix: remove sensitive configuration files from repository

--- FrontEnd/DoctorAppointmentSystem/src/app/core/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: false,
-    recaptcha: {
-      siteKey: '6Ldg8RgrAAAAABIgMEqosV5bv6VTACK6MUVbk1yx'
-    },
-  apiUrl: 'https://doc-net2.runasp.net/api',
-  api_Url: 'https://doc-net2.runasp.net/api'
-   
-  };
