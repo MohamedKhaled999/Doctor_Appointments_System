94a3955f57d681591f7b7c72666e7ca22a153d42
Mohamed Khaled <68979528+MohamedKhaled999@users.noreply.github.com>
2025-07-08 20:43:13 +0300

    Add files via upload

---
+++ FrontEnd/DoctorAppointmentSystem/src/app/core/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    recaptcha: {
+      siteKey: '6Ldg8RgrAAAAABIgMEqosV5bv6VTACK6MUVbk1yx'
+    },
+  apiUrl: 'https://doc-net2.runasp.net/api',
+  api_Url: 'https://doc-net2.runasp.net/api'
+   
+  };
